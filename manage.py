1baf96219a6cd19cba74769c37f96b981c6f46e0
Δημήτρης Φιλίππου <p3160253@dias.aueb.gr>
2017-12-19 14:47:12 +0200

    this boilerplate is now a minimal approach to make APIs

--- manage.py
+++ manage.py
@@ -1,33 +1,15 @@
 """
 This module makes the app runnable with a single command
-
-Available commands:
-> python manage.py runserver
-> python manage.py runcleaner
-> python manage.py runlinter
-
 """
 from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager as Manager_Rugby
-
-from pylint.lint import Run as lint_module
-
+from flask_script import Manager
 from app import app, db
 
-from clean import clean
 
 migrate = Migrate(app, db)
-manager = Manager_Rugby(app)
+manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
-
-@manager.command
-def runcleaner():
-    clean()
-
-@manager.command
-def runlinter():
-    lint_module(['app'])
 
 
 if __name__ == '__main__':
